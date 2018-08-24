0da1a6195c7b73e1f611bd1dfdce453cd4cecf8d
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-08-24 05:18:26 +0300

    Create queries.js

---
+++ aminu/groceristar/queries.js
@@ -0,0 +1,35 @@
+// Department Router
+// more details read here: https://groceristar.github.io/documentation/docs/groceristar-website-methods-list/department-router/department-router.html
+
+// GetDepartmentData(department_id, grocery_id)
+// GetDepartmentsList(grocery_id)
+// ShowAllDepartments(grocery_id)
+// ShowEnabledDepartments(grocery_id)
+
+
+// Favorite Router
+// https://groceristar.github.io/documentation/docs/groceristar-website-methods-list/favorite-router/favorite-router.html
+
+// GetFavoriteIngredients(user_id, department_id)
+// GetAllFavoriteIngredients(user_id)
+
+
+// Grocery Router
+// https://groceristar.github.io/documentation/docs/groceristar-website-methods-list/grocery-router/grocery-router.html
+
+// GetAllGroceryLists()
+// DisplayGroceryListData(grocery_id)
+// DisplayHiddenDepartments(grocery_id)
+// DisplayIngredients(grocery_id, department_id, user_id)
+
+
+// Purchased Router
+// https://groceristar.github.io/documentation/docs/groceristar-website-methods-list/purchased-router/purchased-router.html
+
+// GetPurchasedIngredients(grocery_id, department_id)
+
+
+// Ingredients Router
+// https://groceristar.github.io/documentation/docs/groceristar-website-methods-list/ingredients-router/ingredients-router.html
+
+// GetIngredientList(grocery_id, department_id)
